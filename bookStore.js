84d600c3a87cb8eddf062314daf6c6eac74f2b90
testGit <test@Git.com>
2023-09-28 17:20:44 +0530

    Book Entry

---
+++ bookStore.js
@@ -0,0 +1,40 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Title: ", function (userInputA) {
+  rl.question("Author: ", function (userInputB) {
+    rl.question("Pages: ", function (userInputC) {
+      rl.question("Read? (y/n): ", function (userInputD) {
+        if (userInputD === "y") {
+          isRead = "and is read";
+        } else {
+          isRead = " and is not read yet!";
+        }
+        isPages = userInputC;
+        isAuthor = userInputB;
+        isTitle = userInputA;
+
+        const myBook = new book(isTitle, isAuthor, isPages, isRead);
+        myBook.info();
+
+        rl.close();
+      });
+    });
+  });
+});
+
+function book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    console.log(`${title} by ${author}, is ${pages} pages, ${read}`);
+  };
+}
+
+const book1 = new book("The Harry Potter", "JKRowling", 295, "not read yet!");
